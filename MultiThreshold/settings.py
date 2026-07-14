58baed6dadb12e8e576ccff07f95b0e7fcfe8449
yoavbenyaakov <yoavb1@mail.tu.ac.il>
2026-07-14 11:20:39 +0300

    Debug = False

--- MultiThreshold/settings.py
+++ MultiThreshold/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s6g8q#qc-nxmjuqhuqd+hdpesyt62uqld0h^p$%=h6m-ei*^(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'iwitlab.pythonanywhere.com',
